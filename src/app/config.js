3f49b6ea1c383b9c0e2c4085f77d980753487a78
crystalmanner <crystalmanner130@outlook.com>
2018-12-15 23:59:43 -1200

    add output function

---
+++ src/app/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  // Change this to your firebase configuration! (Add Firebase to your web app)
+    FIREBASE_CONFIG: {
+      apiKey: "AIzaSyBgD4q3YujiPOOt4sPAfBHzBtG6xENp-TE",
+      authDomain: "adwebsite-928a9.firebaseapp.com",
+      databaseURL: "https://adwebsite-928a9.firebaseio.com",
+      projectId: "adwebsite-928a9",
+      storageBucket: "adwebsite-928a9.appspot.com",
+      messagingSenderId: "72060758788"
+    },
+};
